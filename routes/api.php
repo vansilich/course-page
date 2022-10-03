2ab2894dff3ab4a7d38fff2ba2887349d5e25ba0
Lavreek <lavreeksolacki@yandex.ru>
2022-10-03 16:15:05 +0300

    fq :(

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('debug/mail', [DebugMailController::class, 'index']);
+//Route::post('debug/mail', [DebugMailController::class, 'index']);
